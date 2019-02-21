faa136e6248d8cf0299174edfa61f74521b0c405
stephanieDL <35565670+stephanieDL@users.noreply.github.com>
2019-02-21 17:02:10 +0800

    Update wdaproxy.rb

--- Formula/wdaproxy.rb
+++ Formula/wdaproxy.rb
@@ -3,7 +3,7 @@ class Wdaproxy < Formula
   homepage ""
   url "https://github.com/stephanieDL/wdaproxy/archive/0.1.3.tar.gz"
   version "0.1.3"
-  sha256 "638e7b29903b267f7a20944325cfc617bebecd48"
+  sha256 "8c9e80c1e680e039343b53987a1e51ed3fcad5db321ad5dd0845296e29e4facb"
   
   depends_on "usbmuxd"
 

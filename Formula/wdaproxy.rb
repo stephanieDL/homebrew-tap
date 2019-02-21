d5c1cf9f444df4823fdce98341fd070f0d152438
stephanieDL <35565670+stephanieDL@users.noreply.github.com>
2019-02-21 15:57:44 +0800

    Update wdaproxy.rb

--- Formula/wdaproxy.rb
+++ Formula/wdaproxy.rb
@@ -1,7 +1,7 @@
 class Wdaproxy < Formula
   desc ""
   homepage ""
-  url "https://github.com/openatx/wdaproxy/releases/download/0.1.2/wdaproxy_0.1.2_darwin_amd64.tar.gz"
+  url "https://github.com/stephanie/wdaproxy/releases/download/0.1.2/wdaproxy_0.1.2_darwin_amd64.tar.gz"
   version "0.1.2"
   sha256 "1c2cd7ff7bb4626044d7a2d2bcc4d35dc3918fb261787d0ad082fc423b0da072"
   
